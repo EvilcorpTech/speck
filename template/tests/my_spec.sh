b1c08dc2c50ec0d21a2a46dad7f03e777f928c08
Daniele Orlando <dnl.rlnd@gmail.com>
2018-01-08 23:34:01 +0100

    Speck, The Shell Test Runner

---
+++ template/tests/my_spec.sh
@@ -0,0 +1,31 @@
+: ${VAR:=val}
+
+before()
+{
+}
+
+after()
+{
+}
+
+it_should_do_something()
+{
+        test 'a' = 'a'
+}
+
+xit_should_is_disabled()
+{
+}
+
+it_should_do_something()
+{
+        test 'a' = 'a'
+}
+
+test_something()
+{
+}
+
+xtest_something()
+{
+}
